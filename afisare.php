ef5ca8ecd6d539297a4c5398d1a3354d0e914733
CiocanGeorge <43909610+CiocanGeorge@users.noreply.github.com>
2020-08-26 03:00:55 -0700

    Add files via upload

--- afisare.php
+++ afisare.php
@@ -1,8 +1,8 @@
-// selectare preparat doar o data ->  SELECT DISTINCT preparat FROM prepingr
-// selectare ingredient doar o data -> SELECT DISTINCT ingredient FROM prepingr
 
 <!DOCTYPE html>
 <html>
+
+
 	<head>
 		<meta charset="utf-8">
 		<title>Ciocan</title>
@@ -31,39 +31,54 @@
 			}
 			$querycol="SELECT DISTINCT `preparat` FROM `prepingr`";
 			$querylinie="SELECT DISTINCT `ingredient` FROM `prepingr`";
-			$querygram="SELECT DISTINCT `gramaj` FROM `prepingr`";
+			#$querygram="SELECT DISTINCT `gramaj` FROM `prepingr`";
 			$resultcol=mysqli_query($con,$querycol);
 			$resultlinie=mysqli_query($con,$querylinie);
-			$resultgram=mysqli_query($con,$querygram);
+			#$resultgram=mysqli_query($con,$querygram);
 			
 			echo "<tr><th></th>";
-			
+			$lista=array();
 			while($row1=mysqli_fetch_array($resultlinie))
 			{
 				
+				
 				foreach($row1 as $data)
 				{
-					 echo "<th align='center'>". $data . "</th>";
-					 break;
+					$lista[]=$data;
+					echo "<th align='center'>". $data . "</th>";
+					break;
 				}
 				
 			}
 			echo "</tr>";
-			$row3=mysqli_fetch_array($resultgram);
+			
+			
+			
 			while($row2=mysqli_fetch_array($resultcol))
 			{
 				echo "<tr>";
 				foreach($row2 as $data)
 				{
 					 echo "<th align='center'>". $data ."</th>";#. "</th></tr>";
-					 echo $row1[0];
-					 if($row2==$row1)
-					 {
-						 echo "<th>".$data."</th></tr>";
-					 }
+					for($i=0;$i<count($lista);$i=$i+1)
+					{
+					$query1="SELECT DISTINCT `gramaj` FROM `prepingr` WHERE preparat='".$data."' AND ingredient='".$lista[$i]."'";
+					$res=mysqli_query($con,$query1);
+				
+					
+	
+					
+					 if(($row3=mysqli_fetch_array($res))!=0)
+					{
+						echo "<th>".$row3['gramaj']."</th>";
+					}
+					else
+						 echo "<th></th>";
+					}
+					 
 					 break;
 				}
-				
+				echo "</tr>";
 			}
 			
 		
